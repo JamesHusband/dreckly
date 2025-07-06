01402e1b00e431f4dc8845da9de7d4f7e35cf119
James Husband <jchusband@proton.me>
2025-07-06 10:13:34 +0100

    feat: add get restaurant (temp)

--- apps/dreckly/src/app/api/restaurants/[id]/route.ts
+++ apps/dreckly/src/app/api/restaurants/[id]/route.ts
@@ -1,19 +1,25 @@
-import { restaurantData } from '@dreckly/data-access';
+import { getRestaurants } from '@dreckly/data-access';
 
-export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const id = url.pathname.split('/').pop();
-
-  const restaurant = restaurantData.find((r) => String(r.id) === id);
+export const GET = async (
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  const { id } = await params;
+  const restaurantsData = await getRestaurants();
+  const restaurant = restaurantsData.find((r) => r.id.toString() === id);
 
   if (!restaurant) {
     return new Response(JSON.stringify({ error: 'Restaurant not found' }), {
       status: 404,
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
   }
 
   return new Response(JSON.stringify(restaurant), {
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
-}
+};
