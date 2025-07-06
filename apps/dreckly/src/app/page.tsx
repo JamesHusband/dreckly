9ba7da4f3e0df8ccee13e4405446b8a91ca511b4
James Husband <jchusband@proton.me>
2025-07-06 09:46:45 +0100

    chore: get restaurant / cuisine data from updated routes

--- apps/dreckly/src/app/page.tsx
+++ apps/dreckly/src/app/page.tsx
@@ -1,39 +1,19 @@
 import { Hero, CuisineFilter, RestaurantList } from '@dreckly/features-home';
-import { Restaurant } from '@dreckly/shared-types';
-
-import {
-  Utensils,
-  Fish,
-  UtensilsCrossed,
-  Soup,
-  Pizza,
-  Hamburger,
-  Cake,
-  Salad,
-} from 'lucide-react';
+import { Restaurant, Cuisine } from '@dreckly/shared-types';
 
 const Page = async () => {
-  const res = await fetch('http://localhost:3000/api/restaurants', {
-    cache: 'no-store',
-  });
-  const restaurants: Restaurant[] = await res.json();
+  const [restaurantsRes, cuisinesRes] = await Promise.all([
+    fetch('http://localhost:3000/api/restaurants', { cache: 'no-store' }),
+    fetch('http://localhost:3000/api/cuisines', { cache: 'no-store' }),
+  ]);
 
-  console.info('restaurants from page', restaurants);
-  const cuisineTypes = [
-    { name: 'Cornish', icon: Utensils },
-    { name: 'Fish & Chips', icon: Fish },
-    { name: 'Indian', icon: UtensilsCrossed },
-    { name: 'Chinese', icon: Soup },
-    { name: 'Italian', icon: Pizza },
-    { name: 'Burgers', icon: Hamburger },
-    { name: 'Desserts', icon: Cake },
-    { name: 'Healthy', icon: Salad },
-  ];
+  const restaurants: Restaurant[] = await restaurantsRes.json();
+  const cuisines: Cuisine[] = await cuisinesRes.json();
 
   return (
     <>
       <Hero />
-      <CuisineFilter cuisineTypes={cuisineTypes} />
+      <CuisineFilter cuisines={cuisines} />
       <RestaurantList restaurants={restaurants} />
     </>
   );
