559e972ce8f2eedbbbf95cdadeaed9d2bb7ae242
James Husband <jchusband@proton.me>
2025-07-06 13:47:36 +0100

    chore: move BackToRestaurants to ui-kit

--- libs/features/restaurants/src/lib/components/index.ts
+++ libs/features/restaurants/src/lib/components/index.ts
@@ -1,5 +1,4 @@
 export * from './RestaurantHeader';
 export * from './MinimumOrderNotice';
 export * from './MenuSection';
-export * from './BackToRestaurants';
 export * from './OrderSidebar';
--- libs/shared/ui-kit/src/lib/index.ts
+++ libs/shared/ui-kit/src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './Logo';
 export * from './DropdownMenu';
 export * from './NavButton';
 export * from './CuisineCard';
+export * from './BackToRestaurants';
