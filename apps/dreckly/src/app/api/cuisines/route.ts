ea8e33803e21b6ff2dea1e57b611b0bd748df32a
James Husband <jchusband@proton.me>
2025-08-07 14:05:09 +0100

    chore: remove next api endpoints

--- apps/dreckly/src/app/api/cuisines/route.ts
+++
@@ -1,8 +0,0 @@
-import { getCuisines } from '@dreckly/data-access';
-
-export const GET = async (request: Request) => {
-  const cuisines = await getCuisines();
-  return new Response(JSON.stringify(cuisines), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-};
--- apps/dreckly/src/app/api/restaurants/route.ts
+++
@@ -1,8 +0,0 @@
-import { getRestaurants } from '@dreckly/data-access';
-
-export const GET = async (request: Request) => {
-  const restaurants = await getRestaurants();
-  return new Response(JSON.stringify(restaurants), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-};
